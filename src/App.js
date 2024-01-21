13d514f4208255d286bccc1da44c2be0346f6ad3
Eugenio7779 <47753023+Eugenio7779@users.noreply.github.com>
2024-01-21 12:10:46 +0300

    style: add icons pack

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Приключения в стиле JS
+          <br />
+          или как сделать ToDo list
         </p>
         <a
           className="App-link"
