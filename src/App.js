a052601937a47e00705efb28f51479619af36d97
Eugenio7779 <47753023+Eugenio7779@users.noreply.github.com>
2024-01-21 20:48:30 +0300

    feat: add cover and components files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Cover } from './components/Cover';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Приключения в стиле JS
-          <br />
-          или как сделать ToDo list
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Cover/>
     </div>
   );
 }
---
+++ src/components/EditForm.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export const EditForm = () => {
+    return(
+        <div>EditForm</div>
+    )
+}
