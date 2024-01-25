d0a5b944a9f8b51607c61951369ed41e79cd3ccd
Eugenio7779 <47753023+Eugenio7779@users.noreply.github.com>
2024-01-25 23:04:21 +0300

    style: update icon styles

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -7,8 +7,8 @@ export const Tasks = ({task, toggleComplete, deleteTodo, editTodo}) => {
         <div className='Tasks'>
             <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ""}`}>{task.task}</p>
             <div>
-                <FormOutlined onClick={() => editTodo(task.id)} />
-                <DeleteOutlined onClick={() => deleteTodo(task.id)}/>
+                <FormOutlined onClick={() => editTodo(task.id)} className='edit-icon'/>
+                <DeleteOutlined onClick={() => deleteTodo(task.id)} className='delete-icon'/>
             </div>
         </div>
     )
